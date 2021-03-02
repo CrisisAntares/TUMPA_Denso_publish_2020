94f01dceca92c487914f469a68f321a7ef7892d1
hayato <kkudo487771@gmail.com>
2021-03-02 19:51:04 +0900

    add coment

--- fly-by-wire steering system/fly-by-wire steering system.ino
+++ fly-by-wire steering system/fly-by-wire steering system.ino
@@ -1,3 +1,22 @@
+/*このコードを何かに役立てようとしている人へ
+このコードは人力飛行機という飛行機を動かすために作った試作品です。
+基本的な関数、map、analogReadなどの関数を使用しておりサーボモータの動作内容もわかりやすくしています。
+初心者向けのコードがたくさんあるのでうまく使ってください。
+しかし一部コードを書き替えているので何か実践的に使用する場合は使用するモータや可変抵抗に合わせてコードを
+書き直したり試行錯誤して使ってみてください。すべてのモータ、可変抵抗に合わせて作ったものでないのでこのコード
+を使用して危険が生じても作者および共同開発者は責任を持ちませんので注意してください。
+ではよい電子工作ライフを！！
+CrisisAntares
+*/
+/*To those who are trying to make use of this code for something!
+This code is a prototype that I made to run an airplane called a human-powered plane.
+It uses basic functions, map, analogRead, and other functions to make it easy to understand what the servo motors do.
+There is a lot of code for beginners, so please use it well.
+However, some of the code has been rewritten, so if you want to use it in a practical way, please rewrite the code to fit the motor and variable resistor you are using, or use trial and error. Please note that the author and co-developers are not responsible for any danger caused by using this code, as it is not designed for all motors and variable resistors.
+Have a good electronics building life!
+
+Translated with www.DeepL.com/Translator (free version)
+CrisisAntares*/
 #include<Servo.h>
 
 Servo servoel;
